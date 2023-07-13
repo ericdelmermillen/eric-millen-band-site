247883bf9e829588b2921fb7b9a854373e96eea3
Eric <ericdelmermillen@gmail.com>
2023-07-12 20:07:54 -0400

    created function that clears input form fields and logs values for comments section on bio page

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,3 +1,50 @@
-const year = document.getElementById("year");
-    let currentYear = new Date().getFullYear();
-    year.textContent = currentYear;
+const nameInput = document.querySelector('#name');
+const commentInput = document.querySelector('#comment');
+const commentForm = document.getElementById("commentForm");
+const commentBtn = document.getElementById("commentBtn");
+const commentsArticle = document.querySelector(".comments__article")
+
+
+
+function logForm(e) {
+    e.preventDefault();
+    console.log(`name: ${nameInput.value}, comment: ${commentInput.value}`)
+    // commentInput.value = "";
+}
+
+// clear form
+function clearForm(e) {
+    e.preventDefault();
+    nameInput.value = "";
+    commentInput.value = "";
+}
+
+
+commentForm.addEventListener("submit", (e) => {
+    logForm(e);
+    clearForm(e);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const year = document.getElementById("year");
+//     let currentYear = new Date().getFullYear();
+//     year.textContent = currentYear;
